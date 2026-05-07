626895d6a00a666f4cdccdac8fe8ad315a03a4b4
vuvannghiawork <vuvannghia.work@gmail.com>
2026-05-07 12:16:58 +0700

    1

--- env.py
+++ env.py
@@ -3,12 +3,6 @@ import os
 from environs import Env
 from loguru import logger
 
-# log_file_format = "{time:YYYY-MM-DD}.log"
-# logger.add(
-#     f"logging/{log_file_format}", rotation="00:00", retention="7 days", enqueue=True
-# )
-
-
 env = Env()
 logger.info("Loading environment variables...")
 
